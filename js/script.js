0f3fd01661280cadbf926a496d07e69b3c422df5
yaaann-wnd <aftiyangamer45@gmail.com>
2023-03-07 00:34:18 +0700

    add testimoni section

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
 const brand = document.querySelector('.brand');
 const navItem = document.querySelectorAll('.nav-item');
+const package = document.querySelector('.package');
+const call = document.querySelector('.call');
+const button = document.querySelector('button');
 
-window.addEventListener('scroll', function (){
-    const scroll = window.scrollY;
-
-    if (scroll > 636) {
-        brand.style.color = 'var(--primary)';
-
-        navItem.forEach(function(nav){
-            nav.style.color = 'var(--primary)';
-        })
+button.addEventListener('click', function () {  
+    if (window.innerWidth < 600) {
+        window.scrollBy(0, 1193);        
     } else {
-        brand.style.color = 'var(--secondary)';
-        navItem.forEach(function(nav){
-            nav.style.color = 'var(--secondary)';
-        })
-    }
-
-})
-
-window.addEventListener('touchmove', function () {
-    const scroll = window.scrollY;
-
-    if (scroll > 569) {
-        brand.style.color = 'var(--primary)';
-
-        navItem.forEach(function(nav){
-            nav.style.color = 'var(--primary)';
-        })
-    } else {
-        brand.style.color = 'var(--secondary)';
-        navItem.forEach(function(nav){
-            nav.style.color = 'var(--secondary)';
-        })
+        window.scrollBy(0, 600);
     }
 })
